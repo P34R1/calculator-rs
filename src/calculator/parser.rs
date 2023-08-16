2e5ba603557464aa0a82f909322b5470c299b1b0
P34RL <undeadgamer279@gmail.com>
2023-08-16 15:54:37 -0400

    Refactored Parser Implementation

--- src/calculator/parser.rs
+++ src/calculator/parser.rs
@@ -1,4 +1,4 @@
-use crate::calculator::lexer::{Lexer, Token};
+use crate::calculator::lexer::Token;
 use std::{iter::Peekable, slice::Iter};
 
 pub struct Parser<'a> {
@@ -12,23 +12,17 @@ impl<'a> Parser<'a> {
         Parser { tokens_iter }
     }
     pub fn parse_expression(&mut self) -> f64 {
-        self.parse_expression_recursive()
-    }
-
-    fn parse_expression_recursive(&mut self) -> f64 {
-        let mut result = self.parse_term_recursive();
+        let mut result = self.parse_term();
 
         while let Some(&token) = self.tokens_iter.peek() {
             match token {
                 Token::Plus => {
                     self.tokens_iter.next();
-                    let term = self.parse_term_recursive();
-                    result += term;
+                    result += self.parse_term();
                 }
                 Token::Minus => {
                     self.tokens_iter.next();
-                    let term = self.parse_term_recursive();
-                    result -= term;
+                    result -= self.parse_term();
                 }
                 _ => break,
             }
@@ -37,20 +31,18 @@ impl<'a> Parser<'a> {
         result
     }
 
-    fn parse_term_recursive(&mut self) -> f64 {
-        let mut result = self.parse_factor_recursive();
+    fn parse_term(&mut self) -> f64 {
+        let mut result = self.parse_factor();
 
         while let Some(&token) = self.tokens_iter.peek() {
             match token {
                 Token::Multiply => {
                     self.tokens_iter.next();
-                    let factor = self.parse_factor_recursive();
-                    result *= factor;
+                    result *= self.parse_factor();
                 }
                 Token::Divide => {
                     self.tokens_iter.next();
-                    let factor = self.parse_factor_recursive();
-                    result /= factor;
+                    result /= self.parse_factor();
                 }
                 _ => break,
             }
@@ -59,15 +51,14 @@ impl<'a> Parser<'a> {
         result
     }
 
-    fn parse_factor_recursive(&mut self) -> f64 {
+    fn parse_factor(&mut self) -> f64 {
         match self.tokens_iter.next() {
             Some(&Token::Number(value)) => value,
             Some(&Token::LeftParen) => {
-                let expression_value = self.parse_expression_recursive();
-                if let Some(&Token::RightParen) = self.tokens_iter.next() {
-                    expression_value
-                } else {
-                    panic!("Expected ')' after expression");
+                let expression_value = self.parse_expression();
+                match self.tokens_iter.next() {
+                    Some(&Token::RightParen) => expression_value,
+                    _ => panic!("Expected ')' after expression"),
                 }
             }
             _ => panic!("Unexpected token"),
@@ -79,18 +70,28 @@ impl<'a> Parser<'a> {
 mod tests {
     use crate::calculator::{lexer::Lexer, parser::Parser};
 
-    #[test]
-    fn test_parser() {
-        let input = "3.2 + 5 * (10 - 2)";
-        let expected_answer = 43.2;
-
+    fn parse(input: &str) -> f64 {
         let mut lexer = Lexer::new(input);
         let binding = lexer.get_tokens();
 
         let mut parser = Parser::new(&binding);
 
-        let actual_answer = parser.parse_expression();
+        parser.parse_expression()
+    }
 
-        assert_eq!(expected_answer, actual_answer);
+    #[test]
+    fn test_parser() {
+        let inputs = [
+            "3.2 + 5 * (10 - (2 + 3))",
+            "32 - 7 * (104 / 2 + 3)",
+            "6 * 4 - (10 - 2) / 2",
+            "((312) + 32 * 0.5) - 14 / 2",
+        ];
+
+        let expected_answers = [28.2, -353.0, 20.0, 321.0];
+
+        for (input, expected_answer) in inputs.iter().zip(expected_answers.iter()) {
+            assert_eq!(expected_answer, &parse(input));
+        }
     }
 }
