32ab4af0adab2ad90793303698ef3bfddebd5b2f
P34RL <undeadgamer279@gmail.com>
2023-08-16 15:13:54 -0400

    Wrote Parser

--- src/calculator/parser.rs
+++ src/calculator/parser.rs
@@ -1,26 +1,85 @@
 use crate::calculator::lexer::{Lexer, Token};
-pub struct Parser {}
-//
-impl Parser {
-    pub fn new(lexer: Lexer) -> Self {
-        Parser {}
+use std::{error::Error, iter::Peekable};
+
+pub fn parse_expression(tokens: &[Token]) -> f64 {
+    let mut tokens_iter = tokens.iter().peekable();
+    parse_expression_recursive(&mut tokens_iter)
+}
+
+fn parse_expression_recursive<'a>(
+    tokens_iter: &mut Peekable<impl Iterator<Item = &'a Token>>,
+) -> f64 {
+    let mut result = parse_term_recursive(tokens_iter);
+
+    while let Some(&token) = tokens_iter.peek() {
+        match token {
+            Token::Plus => {
+                tokens_iter.next();
+                let term = parse_term_recursive(tokens_iter);
+                result += term;
+            }
+            Token::Minus => {
+                tokens_iter.next();
+                let term = parse_term_recursive(tokens_iter);
+                result -= term;
+            }
+            _ => break,
+        }
     }
 
-    pub fn eat(&mut self, expected_token: Token) {}
+    result
+}
 
-    pub fn factor(&mut self) -> f64 {
-        0.0
+fn parse_term_recursive<'a>(tokens_iter: &mut Peekable<impl Iterator<Item = &'a Token>>) -> f64 {
+    let mut result = parse_factor_recursive(tokens_iter);
+
+    while let Some(&token) = tokens_iter.peek() {
+        match token {
+            Token::Multiply => {
+                tokens_iter.next();
+                let factor = parse_factor_recursive(tokens_iter);
+                result *= factor;
+            }
+            Token::Divide => {
+                tokens_iter.next();
+                let factor = parse_factor_recursive(tokens_iter);
+                result /= factor;
+            }
+            _ => break,
+        }
     }
 
-    pub fn term(&mut self) -> f64 {
-        0.0
-    }
+    result
+}
 
-    pub fn expression(&mut self) -> f64 {
-        0.0
+fn parse_factor_recursive<'a>(tokens_iter: &mut Peekable<impl Iterator<Item = &'a Token>>) -> f64 {
+    match tokens_iter.next() {
+        Some(&Token::Number(value)) => value,
+        Some(&Token::LeftParen) => {
+            let expression_value = parse_expression_recursive(tokens_iter);
+            if let Some(&Token::RightParen) = tokens_iter.next() {
+                expression_value
+            } else {
+                panic!("Expected ')' after expression");
+            }
+        }
+        _ => panic!("Unexpected token"),
     }
+}
 
-    pub fn parse(&mut self) -> f64 {
-        0.0
+#[cfg(test)]
+mod tests {
+    use crate::calculator::{lexer::Lexer, parser::parse_expression};
+
+    #[test]
+    fn test_parser() {
+        let input = "3.2 + 5 * (10 - 2)";
+        let expected_answer = 43.2;
+
+        let mut lexer = Lexer::new(input);
+
+        let actual_answer = parse_expression(&lexer.get_tokens());
+
+        assert_eq!(expected_answer, actual_answer);
     }
 }
